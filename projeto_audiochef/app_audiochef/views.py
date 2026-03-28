212e99b4354d0f18f23274b95c4e9db1b1cc4b2d
Caique Brito <gameroff12345@gmail.com>
2026-03-28 09:29:58 -0300

    feat: view RecipesAPIView completed

--- projeto_audiochef/app_audiochef/views.py
+++ projeto_audiochef/app_audiochef/views.py
@@ -1,5 +1,7 @@
 import requests
 import os
+from rest_framework.response import Response
+from rest_framework import status
 from dotenv import load_dotenv
 from django.shortcuts import render
 from rest_framework import viewsets
@@ -13,27 +15,30 @@ def home (request):
     return render(request, 'app_audiochef/index.html')
 
 class RecipesAPIView(APIView):
-    BASE_URL = "https://api.spoonacular.com/recipes/random"
+    def get(self, request):
+        BASE_URL = "https://api.spoonacular.com/recipes/random"
 
-    params = {
-        "apiKey": os.getenv("API_KEY"),
-        "number": 1
-    }
-
-    response = requests.get(BASE_URL, params=params)
-    data = response.json()
-
-    recipes = data["recipes"][0]
-
-    result = {
-        "diets": recipes["diets"],
-        "dishTypes": recipes["dishTypes"],
-        "extendedIngredients": recipes["extendedIngredients"],
-        "image": recipes["image"],
-        "instructions": recipes["instructions"],
-        "summary": recipes["summary"],
-        "vegan": recipes["vegan"],
-        "vegetarian": recipes["vegetarian"],
-        "servings": recipes["servings"],
-        "readyInMinutes": recipes["readyInMinutes"],
+        params = {
+            "apiKey": os.getenv("API_KEY"),
+            "number": 1
         }
+
+        response = requests.get(BASE_URL, params=params)
+        data = response.json()
+
+        recipes = data["recipes"][0]
+
+        result = {
+            "diets": recipes["diets"],
+            "dishTypes": recipes["dishTypes"],
+            "extendedIngredients": recipes["extendedIngredients"],
+            "image": recipes["image"],
+            "instructions": recipes["instructions"],
+            "summary": recipes["summary"],
+            "vegan": recipes["vegan"],
+            "vegetarian": recipes["vegetarian"],
+            "servings": recipes["servings"],
+            "readyInMinutes": recipes["readyInMinutes"],
+        }
+
+        return Response(result, status=status.HTTP_200_OK)
