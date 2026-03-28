dcb7ab0148e8aa1eff99202ea4ae2a512be717a4
Caique Brito <gameroff12345@gmail.com>
2026-03-27 22:49:01 -0300

    feat: add urls, serializers on app, configured initials routes and views

--- projeto_audiochef/app_audiochef/views.py
+++ projeto_audiochef/app_audiochef/views.py
@@ -1,3 +1,10 @@
+import requests
 from django.shortcuts import render
+from rest_framework import viewsets
+from rest_framework.views import APIView
+from .models import *
+from . import serializers
 
 # Create your views here.
+def home (request):
+    return render(request, 'app_audiochef/index.html')
--- projeto_audiochef/projeto_audiochef/settings.py
+++ projeto_audiochef/projeto_audiochef/settings.py
@@ -37,6 +37,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'app_audiochef',
+    'rest_framework',
 ]
 
 MIDDLEWARE = [
