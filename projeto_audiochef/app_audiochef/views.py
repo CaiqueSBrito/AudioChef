ab8f90232c54a918d5e307712d4e12ed70aace49
Caique Brito <gameroff12345@gmail.com>
2026-03-27 23:32:57 -0300

    feat: partial configured RecipesAPI views

--- projeto_audiochef/app_audiochef/views.py
+++ projeto_audiochef/app_audiochef/views.py
@@ -1,10 +1,39 @@
 import requests
+import os
+from dotenv import load_dotenv
 from django.shortcuts import render
 from rest_framework import viewsets
 from rest_framework.views import APIView
 from .models import *
 from . import serializers
+load_dotenv()
 
 # Create your views here.
 def home (request):
     return render(request, 'app_audiochef/index.html')
+
+class RecipesAPIView(APIView):
+    BASE_URL = "https://api.spoonacular.com/recipes/random"
+
+    params = {
+        "apiKey": os.getenv("API_KEY"),
+        "number": 1
+    }
+
+    response = requests.get(BASE_URL, params=params)
+    data = response.json()
+
+    recipes = data["recipes"][0]
+
+    result = {
+        "diets": recipes["diets"],
+        "dishTypes": recipes["dishTypes"],
+        "extendedIngredients": recipes["extendedIngredients"],
+        "image": recipes["image"],
+        "instructions": recipes["instructions"],
+        "summary": recipes["summary"],
+        "vegan": recipes["vegan"],
+        "vegetarian": recipes["vegetarian"],
+        "servings": recipes["servings"],
+        "readyInMinutes": recipes["readyInMinutes"],
+        }
